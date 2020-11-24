daa6e754b0e93151d7c9e3c23fad9dd656f07a2c
Maxim Kirshin <meowto16@gmail.com>
2020-11-24 09:46:43 +0500

    lesson 33

--- src/app/Repositories/BlogPostRepository.php
+++ src/app/Repositories/BlogPostRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repositories;
 
 use App\Models\BlogPost as Model;
+use Illuminate\Pagination\LengthAwarePaginator;
 
 class BlogPostRepository extends CoreRepository
 {
@@ -13,5 +14,33 @@ class BlogPostRepository extends CoreRepository
     protected function getModelClass()
     {
         return Model::class;
+    }
+
+    /**
+     * Получить список статей для вывода в списке
+     * (Админка)
+     *
+     * @param int $paginateCount - Количество страниц.
+     *
+     * @return LengthAwarePaginator
+     */
+    public function getAllWithPaginate($paginateCount = 25)
+    {
+        $columns = [
+            'id',
+            'title',
+            'slug',
+            'is_published',
+            'published_at',
+            'user_id',
+            'category_id'
+        ];
+
+        $result = $this->startConditions()
+            ->select($columns)
+            ->orderBy('id', 'DESC')
+            ->paginate($paginateCount);
+
+        return $result;
     }
 }
